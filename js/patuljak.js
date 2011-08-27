140b846078c58e0205738ac9c6d972cb6b8e5107
Viktor Kerkez <alefnula@gmail.com>
2011-08-28 00:26:43 +0200

    Added versioning

--- js/patuljak.js
+++ js/patuljak.js
@@ -31,7 +31,7 @@ Patuljak.prototype._read = function _read(fd, position, callback) {
             });
         });
     } else {
-        callback();
+        if (callback) { callback(); }
     }
     
 }
@@ -56,19 +56,17 @@ Patuljak.prototype.writeHeaders = function writeHeaders(headers, callback) {
     var self = this;
     
     var buffer = new Buffer(self.HEADERS_SIZE);
-    buffer.writeUInt32BE(0, 0);                   // crc
-    buffer.writeUInt32BE(0, 4);                   // timestamp
-    buffer.writeUInt32BE(0, 8);                   // version
-    buffer.writeUInt32BE(0, 12);                  // previous ptr
-    buffer.writeUInt16BE(headers.key_size, 16);   // key size
-    buffer.writeUInt32BE(headers.value_size, 18); // value size
+    buffer.writeUInt32BE(0, 0);                     // crc
+    buffer.writeUInt32BE(0, 4);                     // timestamp
+    buffer.writeUInt32BE(headers.version,       8); // version
+    buffer.writeUInt32BE(headers.previous_ptr, 12); // previous ptr
+    buffer.writeUInt16BE(headers.key_size,     16); // key size
+    buffer.writeUInt32BE(headers.value_size,   18); // value size
     
     headers.position = self.position;
     fs.write(self.fd, buffer, 0, self.HEADERS_SIZE, self.position, function (err, written, buffer) {
         self.position += self.HEADERS_SIZE;
-        if (callback) {
-            callback(headers);
-        };
+        if (callback) { callback(headers); };
     });
 };
 
@@ -76,20 +74,23 @@ Patuljak.prototype.readHeaders = function readHeaders(fd, position, callback) {
     fs.read(fd, new Buffer(this.HEADERS_SIZE), 0, this.HEADERS_SIZE, position, function (err, bytesRead, buffer) {
         var crc = buffer.readUInt32BE(0)
           , headers = {
+            'position'     : position,
             'timestamp'    : buffer.readUInt32BE(4),
             'version'      : buffer.readUInt32BE(8),
             'previous_ptr' : buffer.readUInt32BE(12),
             'key_size'     : buffer.readUInt16BE(16),
             'value_size'   : buffer.readUInt32BE(18)
         };
-        headers.position = position;
         callback(headers);
     });
 };
 
 
 Patuljak.prototype.get = function get(key, callback) {
+    if (callback === undefined) { return null; }
+    
     var self = this;
+    
     var headers = self.keyStore[key] || null;
     if (headers === null) {
         return callback(null);
@@ -102,21 +103,28 @@ Patuljak.prototype.get = function get(key, callback) {
     }
 };
 
+
 Patuljak.prototype.put = function put(key, value, callback) {
     var self = this;
-    var headers = self.keyStore[key] || {'version': 0, 'previous_ptr': 0}
-    var bkey = Buffer(key, 'utf8');
-    var bvalue = Buffer(value, 'utf8');
-    headers['key_size'] = bkey.length;
-    headers['value_size'] = bvalue.length;
+    
+    // Setup new headerss
+    var headers = self.keyStore[key];
+    if (headers == undefined) {
+        headers = {'version': 0, 'previous_ptr': 0 };
+    } else {
+        headers.version += 1;
+        headers.previous_ptr = headers.position;
+    }
+    headers.timestamp  = Date.now();
+    headers.key_size   = Buffer(key, 'utf8').length;
+    headers.value_size = Buffer(value, 'utf8').length;
+    
     self.writeHeaders(headers, function (headers) {
         self.keyStore[key] = headers;
-        fs.write(self.fd, bkey, 0, bkey.length, self.position, function (err, written, buffer) {
-            self.position += bkey.length;
-            fs.write(self.fd, bvalue, 0, bvalue.length, self.position, function (err, written, buffer) {
-                self.position += bvalue.length;
-                callback();
-            });
+        var buf = Buffer(key + value);
+        fs.write(self.fd, buf, 0, buf.length, self.position, function (err, written, buffer) {
+            self.position += buf.length;
+            if (callback) { callback(); }
         });
     });
 }
